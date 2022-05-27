0ceb008f9a225f1aec75110002737210d1b6f3e2
2JasonD <98455401+2JasonD@users.noreply.github.com>
2022-05-28 01:33:22 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HMTL_CSS_ASP_NET_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
